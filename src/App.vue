f5bd5a5bd727b6d432a8268043baebb42ea8ab11
KaziJahurul Islam <jahurul.sust@gmail.com>
2020-04-09 13:28:54 +0600

    change dummy text to original text

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
                       <router-link to="/about">Recruiters</router-link>
                       <router-link to="/about">Log in</router-link>
                       <router-link to="/about">Employer / Post job</router-link>
-                      <router-link class="nav-btn" to="/about">Get Alert</router-link>
+                      <router-link class="nav_btn job_btn" to="/about">Get Alert</router-link>
                   </nav>
 
               </div> <!-- /#nav -->
@@ -25,7 +25,116 @@
           </div>
       </div>
 
-    <router-view />
+      <router-view />
+
+      <footer class="site_footer">
+
+          <div class="job_container">
+
+              <div class="footer_col">
+
+                  <h3 class="footer_title">
+                      About Us
+                  </h3>
+
+                  <ul class="footer_nav">
+                      <li>
+                          <a href="#">About Us</a>
+                      </li>
+                      <li>
+                          <a href="#">Terms & Conditions</a>
+                      </li>
+                      <li>
+                          <a href="#">International Partners</a>
+                      </li>
+                      <li>
+                          <a href="#">Other Partners</a>
+                      </li>
+                      <li>
+                          <a href="#">Privacy Policy</a>
+                      </li>
+                      <li>
+                          <a href="#">Feedback</a>
+                      </li>
+                      <li>
+                          <a href="#">Contact Us</a>
+                      </li>
+                  </ul> <!-- /.footer_nav -->
+
+              </div> <!-- /.footer_col -->
+
+              <div class="footer_col">
+
+                  <h3 class="footer_title">
+                      Job Seekers
+                  </h3>
+
+                  <ul class="footer_nav">
+                      <li>
+                          <a href="#">Create Account</a>
+                      </li>
+                      <li>
+                          <a href="#">Career Counseling</a>
+                      </li>
+                      <li>
+                          <a href="#">FAQ</a>
+                      </li>
+                      <li>
+                          <a href="#">Video Guides</a>
+                      </li>
+                  </ul> <!-- /.footer_nav -->
+
+              </div> <!-- /.footer_col -->
+
+              <div class="footer_col">
+
+                  <h3 class="footer_title">
+                      Employer
+                  </h3>
+
+                  <ul class="footer_nav">
+                      <li>
+                          <a href="#">Create Account</a>
+                      </li>
+                      <li>
+                          <a href="#">Products/Service</a>
+                      </li>
+                      <li>
+                          <a href="#">Post a Job</a>
+                      </li>
+                      <li>
+                          <a href="#">FAQ</a>
+                      </li>
+                  </ul> <!-- /.footer_nav -->
+
+              </div> <!-- /.footer_col -->
+
+              <div class="footer_col">
+
+                  <h3 class="footer_title">
+                      Social Media
+                  </h3>
+
+                  <ul class="footer_nav">
+                      <li>
+                          <a href="#">Facebook</a>
+                      </li>
+                      <li>
+                          <a href="#">Google</a>
+                      </li>
+                      <li>
+                          <a href="#">Youtube</a>
+                      </li>
+                      <li>
+                          <a href="#">LinkedIn</a>
+                      </li>
+                  </ul> <!-- /.footer_nav -->
+
+              </div> <!-- /.footer_col -->
+
+          </div>
+
+      </footer>
 
   </div>
 </template>
@@ -48,23 +157,39 @@
         text-decoration: none;
         transition: all .4s ease;
     }
+    .job_btn{
+        display: inline-block;
+        text-align: center;
+        font-weight: 700;
+        background: #365899;
+        color: #fff;
+        border-radius: 5px;
+        padding: 10px 20px;
+        &:hover{
+            background: #6f9ae8;
+        }
+    }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 
+/* nav css start */
+    .desktop-nav{
+        position: relative;
+    }
 #nav {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 20px 0;
+    padding: 15px 0;
     .logo{
         .logo_link{
             display: inline-block;
         }
         .logo_img{
-            height: 54px;
+            height: 50px;
         }
     }
     .right-nav{
@@ -79,13 +204,39 @@
             &.router-link-exact-active {
                 color: #365899;
             }
-            &.nav-btn{
-                background: #365899;
+            &.nav_btn{
                 color: #fff;
-                border-radius: 5px;
-                padding: 10px 20px;
             }
         }
     }
 }
+
+
+    /* footer css start*/
+    .site_footer{
+        padding: 50px 0;
+        background: #365899;
+        .job_container{
+            display: flex;
+            .footer_col{
+                width: 25%;
+            }
+        }
+        .footer_title{
+            color: #fff;
+            margin: 0 0 20px;
+        }
+        .footer_nav{
+            padding: 0;
+            margin: 0;
+            list-style-type: none;
+            li{
+                margin-bottom: 5px;
+                a{
+                    display: inline-block;
+                    color: #c8c8c8;
+                }
+            }
+        }
+    }
 </style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,5 +1,8 @@
 <template>
-    $END$
+    <div class="job_search">
+        <input type="text" placeholder="Search Jobs ...">
+        <button>Search</button>
+    </div>
 </template>
 
 <script>
@@ -8,6 +11,38 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.job_search{
+    display: flex;
+    input{
+        width: calc(100% - 100px);
+        font-size: 16px;
+        height: 46px;
+        padding: 0 20px;
+        border-radius: 0;
+        display: inline-block;
+        background: #e2e2e2;
+        border: none;
+        &:active, &:focus, &:hover{
+            outline: none;
+        }
+    }
+    button{
+        width: 100px;
+        border-radius: 0;
+        display: inline-block;
+        border: none;
+        box-shadow: none;
+        outline: none;
+        background: #6f9ae8;
+        color: #fff;
+        font-size: 18px;
+        text-align: center;
+        cursor: pointer;
+        transition: all .4s ease;
+        &:hover{
+            background: #365899;
+        }
+    }
+}
 </style>
