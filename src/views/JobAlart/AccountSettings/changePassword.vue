37680bbdcc143e878a21b991a4a66f2e087ff510
ahsanriyad <riyad298@gmail.com>
2020-09-13 18:00:08 +0600

    working on change password

--- src/views/JobAlart/AccountSettings/changePassword.vue
+++ src/views/JobAlart/AccountSettings/changePassword.vue
@@ -1,53 +1,71 @@
 <template>
-<div class="change_pass">
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="12">
-        <h1 class="text-center ja__headline">Change password for <span>farzanakabirrinky@yahoo.com</span></h1>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card class="ja__card">
-          <v-form ref="form">
-            <v-col cols="12" md="8" class="col-1 mb-n4 pb-0">
-              <p class="mb-1">Current password</p>
-              <v-text-field
+  <div class="change_pass">
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12">
+          <h1 class="text-center ja__headline">
+            Change password for
+            <span>farzanakabirrinky@yahoo.com</span>
+          </h1>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-card class="ja__card">
+            <v-form ref="form">
+              <v-col cols="12" md="8" class="col-1 mb-n4 pb-0">
+                <p class="mb-1">Current password</p>
+                <v-text-field
                   :rules="[v=>!!v||'required']"
                   class="mb-0 pb-0 mb-0"
                   placeholder="Password"
                   outlined
                   dense
-              ></v-text-field>
-            </v-col>
+                  v-model="data.new_password"
+                ></v-text-field>
+              </v-col>
 
-            <v-col cols="12" md="8" class="col-2 mb-0 pb-0">
-              <p class="mb-1">New password</p>
-              <v-text-field
+              <v-col cols="12" md="8" class="col-2 mb-0 pb-0">
+                <p class="mb-1">New password</p>
+                <v-text-field
                   class="mb-0"
                   :rules="[v=>!!v||'required']"
                   placeholder="Password"
                   outlined
                   dense
-              ></v-text-field>
-            </v-col>
-            <div class="ja__button">
-              <v-btn color="#00204e" class="white--text ma-2" @click.stop="submit">save</v-btn>
-              <v-btn color="#00204e" class="white--text ma-2" link to="/account-settings">cancel changes</v-btn>
-            </div>
-          </v-form>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
-</div>
+                  v-model="data.password"
+                ></v-text-field>
+              </v-col>
+              <div class="ja__button">
+                <v-btn color="#00204e" class="white--text ma-2" @click.stop="submit">save</v-btn>
+                <v-btn
+                  color="#00204e"
+                  class="white--text ma-2"
+                  link
+                  to="/account-settings"
+                >cancel changes</v-btn>
+              </div>
+            </v-form>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
-name: "changePassword",
-  methods:{
-    submit(){
+  name: "changePassword",
+  data: () => {
+    return {
+      data: {
+        password: "",
+        new_password: "",
+      },
+    };
+  },
+  methods: {
+    submit() {
       this.$refs.form.validate();
-    }
-  }
-}
+    },
+  },
+};
 </script>
