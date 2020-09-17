0d8ff7926eb1e4dead93332025ffcb0c500b14c1
farzanakabir <farzanakabirrinky05@gmail.com>
2020-09-17 18:25:02 +0600

    job post api integrated

--- src/views/Employers/Jobs/AddJobs.vue
+++ src/views/Employers/Jobs/AddJobs.vue
@@ -25,11 +25,58 @@
             ></ckeditor>
           </div>
 
+          <div class="d-flex">
+            <div>
+              <p>Job Facilities Title</p>
+              <div class="skill_badge we-skills-box" v-for="(i) in jobFacilitiesArr" :key="i">
+                <div v-if="jobFacilitiesArr.length > 1" class="skill_badge_close" @click.stop="()=>remove(i)">x <span
+                    class="closeButton__tooltip_text">remove</span></div>
+                <div class="d-flex">
+                  <v-text-field
+                      background-color="white"
+                      class="mb-0"
+                      :rules="[v =>!!v||'required']"
+                      placeholder="Enter Job Facilities"
+                      v-model="jobs.job_facilities.title"
+                      outlined
+                      dense
+                  ></v-text-field>
+
+                </div>
+              </div>
+            </div>
+            <div>
+              <p>Job Facilities Text</p>
+              <div class="skill_badge we-skills-box" v-for="(i) in jobResArr" :key="i">
+                <div v-if="jobResArr.length > 1" class="skill_badge_close" @click.stop="()=>remove(i)">x <span
+                    class="closeButton__tooltip_text">remove</span></div>
+                <div class="d-flex">
+                  <v-text-field
+                      background-color="white"
+                      class="mb-0"
+                      :rules="[v =>!!v||'required']"
+                      placeholder="Enter Job Facilities"
+                      v-model="jobs.job_facilities.title"
+                      outlined
+                      dense
+                  ></v-text-field>
+
+                </div>
+              </div>
+            </div>
+            <!--    :disabled="R.isEmpty(job_responsibilities[job_responsibilities.length-1].job_title) "-->
+            <v-btn
+
+                class="add_more_btn ml-5"
+                color="primary"
+                @click.stop="addJobResponsibilities"
+            >Add More+
+            </v-btn>
+          </div>
           <div>
             <p>Job Responsibilities</p>
             <div>
-              <div class="skill_badge we-skills-box" v-for="(jobRes , i) in jobResArr" :key="i">
-                {{ v }}
+              <div class="skill_badge we-skills-box" v-for="(v , i) in jobResArr" :key="i">
                 <div v-if="jobResArr.length > 1" class="skill_badge_close" @click.stop="()=>remove(i)">x <span
                     class="closeButton__tooltip_text">remove</span></div>
                 <div class="d-flex">
@@ -38,7 +85,36 @@
                       class="mb-0"
                       :rules="[v=>!!v||'required']"
                       placeholder="Enter Job Responsibilities"
-                      v-model="jobRes.text"
+                      v-model="v.job_responsibilities"
+                      outlined
+                      dense
+                  ></v-textarea>
+
+                </div>
+              </div>
+            </div>
+            <!--    :disabled="R.isEmpty(job_responsibilities[job_responsibilities.length-1].job_title) "-->
+            <v-btn
+
+                class="add_more_btn ml-5"
+                color="primary"
+                @click.stop="addJobResponsibilities"
+            >Add More+
+            </v-btn>
+          </div>
+          <div>
+            <p>Job Responsibilities</p>
+            <div>
+              <div class="skill_badge we-skills-box" v-for="(v , i) in jobResArr" :key="i">
+                <div v-if="jobResArr.length > 1" class="skill_badge_close" @click.stop="()=>remove(i)">x <span
+                    class="closeButton__tooltip_text">remove</span></div>
+                <div class="d-flex">
+                  <v-textarea
+                      background-color="white"
+                      class="mb-0"
+                      :rules="[v=>!!v||'required']"
+                      placeholder="Enter Job Responsibilities"
+                      v-model="v.job_responsibilities"
                       outlined
                       dense
                   ></v-textarea>
@@ -68,15 +144,58 @@
               ></v-text-field>
             </div>
           </div>
-          <div>
-            <p>Job Type</p>
+          <div class="d-flex justify-space-between">
             <div>
-              <v-select
-                  :menu-props="{ maxHeight: '400' }"
-                  label="Select"
-                  hint="Pick your favorite states"
-                  persistent-hint
-              ></v-select>
+              <p>Job Type</p>
+              <div>
+                <v-select
+                    :menu-props="{ maxHeight: '400' }"
+                    label="Select Job Type"
+                    hint="Pick Job Type"
+                    persistent-hint
+                    :items="types"
+                    :rules="[v=>!!v||'required']"
+                    dense
+                    outlined
+                    background-color="white"
+                ></v-select>
+              </div>
+            </div>
+            <div>
+              <p>Job Category</p>
+              <div>
+                <v-select
+                    item-text="name"
+                    item-value="id"
+                    :items="categories"
+                    label="Select Job Category"
+                    v-model="jobs.job_category.name"
+                    dense
+                    outlined
+                    hint="Pick Job Category"
+                    persistent-hint
+                    background-color="white"
+                    :rules="[v=>!!v||'required']"
+                ></v-select>
+              </div>
+            </div>
+            <div class="item-1">
+              <p>Country</p>
+              <div>
+                <v-select
+                    item-text="country_name"
+                    item-value="id"
+                    :items="countries"
+                    label="Select"
+                    v-model="jobs.country"
+                    :rules="[v=>!!v||'required']"
+                    dense
+                    outlined
+                    hint="Pick Country"
+                    persistent-hint
+                    background-color="white"
+                ></v-select>
+              </div>
             </div>
           </div>
 
@@ -111,19 +230,7 @@
                 ></v-text-field>
               </div>
             </div>
-            <div class="item-1">
-              <p>Country</p>
-              <div>
-                <v-text-field
-                    background-color="white"
-                    class="mb-0"
-                    :rules="[v=>!!v||'required']"
-                    placeholder="Enter your country"
-                    outlined
-                    dense
-                ></v-text-field>
-              </div>
-            </div>
+
           </div>
           <div class="d-flex justify-space-between">
             <div class="item-2" style="width: 50%;">
@@ -137,6 +244,7 @@
                     outlined
                     dense
                     type="number"
+                    v-model="jobs.min_salary_range"
                 ></v-text-field>
               </div>
             </div>
@@ -151,31 +259,29 @@
                     outlined
                     dense
                     type="number"
+                    v-model="jobs.max_salary_range"
                 ></v-text-field>
               </div>
             </div>
           </div>
-
-          <div>
-            <div class="item-1">
-              <p>Mobile Number</p>
-              <div>
-                <vue-tel-input
-                    inputClasses="vTelInput"
-                ></vue-tel-input>
-              </div>
-            </div>
-
-            <div class="item-2">
-              <p>Optional Number</p>
-              <div>
-                <vue-tel-input-vuetify label="Outlined"
-                                       single-line
-                                       outlined dense v-model="phone"></vue-tel-input-vuetify>
-              </div>
+          <div class="item-1">
+            <p>Skills</p>
+            <div>
+              <v-select
+                  item-text="title"
+                  item-value="id"
+                  :items="skills"
+                  placeholder="Select skills"
+                  v-model="jobs.skills.title"
+                  :rules="[v=>!!v||'required']"
+                  dense
+                  outlined
+                  multiple
+                  return-object
+                  background-color="white"
+              ></v-select>
             </div>
           </div>
-
           <div class="text-right">
             <div class="item-2">
               <v-btn color="#365899" class="white--text" :loading="loading" @click.stop="saveJob">Save</v-btn>
@@ -199,6 +305,8 @@ export default {
     return {
       loading: false,
       phone: null,
+      countries: [],
+      types: ['Part-time job', 'Full time job', 'Time flexible', 'Remote'],
       jobs: {
         job_title: '',
         job_description: '',
@@ -210,19 +318,30 @@ export default {
         city: '',
         min_salary_range: '',
         max_salary_range: '',
-        job_category: '',
-        job_educational_req: '',
-        job_facilities: '',
-        job_responsibilities: '',
-        skills: ''
+        job_category: [
+          {id: '', name: ''}
+        ],
+        job_educational_req: [
+          {id: '', degre_title: ''}
+        ],
+        job_facilities: [
+          {title: '', text: ''}
+        ],
+        job_responsibilities: [],
+        skills: [{title: ''}]
       },
       categories: [],
-      skillArray: [],
+      skills: [],
+      jobFacilitiesArr: [],
       jobResArr: [
         {id: '', text: '', is_additional: false}
       ],
       jobResId: "",
       job_responsibilities: '',
+      job_facilities: {
+        title: '',
+        text: ''
+      },
       editor: ClassicEditor,
       editorData: "<p>Content of the editor.</p>",
       editorConfig: {
@@ -235,6 +354,27 @@ export default {
   mounted() {
     this.$store
         .dispatch("callApi", {
+          url: "resume/skill/lists/",
+          method: "get",
+          data: {},
+        })
+        .then((response) => {
+          console.log("skills..... ", response);
+          this.skills = response;
+        })
+    this.$store
+        .dispatch("callApi", {
+          url: "resume/",
+          method: "get",
+          data: {},
+        })
+        .then((response) => {
+          console.log("countries..... ", response);
+          // eventBus.$emit( "fillData" , response.data );
+          this.countries = response.data.countryList;
+        })
+    this.$store
+        .dispatch("callApi", {
           url: "jobs-category/",
           method: "get",
           data: {},
@@ -242,7 +382,7 @@ export default {
         .then((response) => {
           console.log("job category..... ", response);
           // eventBus.$emit( "fillData" , response.data );
-          this.job_category = response.data;
+          this.categories = response.data;
         })
   },
   methods: {
@@ -252,16 +392,11 @@ export default {
           .dispatch("callApi", {
             url: "jobs/",
             method: "post",
-            data : this.jobs,
+            data: this.jobs,
           })
           .then((response) => {
-            console.log("resume ... ff ", response);
-            this.$awn.success("Updated!");
-
-            // this.$refs.form.reset();
-            //saves the items from the database in the table
-            //  console.log(response);
-            //  this.items = response.data;
+            console.log("jobs post done ", response);
+            this.$awn.success("Saved!");
           })
           .catch(() => {
             this.$awn.alert("Failed!");
@@ -269,6 +404,10 @@ export default {
           .finally(() => {
             this.loading = false;
           });
+    },
+    addJobFacilities(){
+      this.jobFacilitiesArr.push(this.job_facilities)
+      this.job_facilities = ''
     },
     addJobResponsibilities() {
       this.jobResArr.push(this.job_responsibilities)
