44e76aba5cf91d7bda64187d433e542e82e297c6
ahsanriyad <riyad298@gmail.com>
2020-10-07 12:27:23 +0600

    toc checkbox added

--- src/views/JobAlart/Auth/Signup.vue
+++ src/views/JobAlart/Auth/Signup.vue
@@ -10,12 +10,14 @@
 
             <v-card>
               <v-row justify="center">
-                <p class="text-center mb-n12 mt-4 header-text">Create a new account</p>
+                <p class="text-center mb-n12 mt-4 header-text">
+                  Create a new account
+                </p>
 
                 <v-col cols="10" md="10" class="col-1 pb-0">
                   <p class="mb-1">Username</p>
                   <v-text-field
-                    :rules="fieldRulesProp(true, 'name' , 'name')"
+                    :rules="fieldRulesProp(true, 'name', 'name')"
                     class="mb-0 pb-0 mb-0"
                     placeholder="Username"
                     outlined
@@ -28,7 +30,7 @@
                 <v-col cols="10" md="10" class="col-2 mb-n4 pb-0">
                   <p class="mb-1">Email</p>
                   <v-text-field
-                    :rules="fieldRulesProp(true, 'email' , 'email')"
+                    :rules="fieldRulesProp(true, 'email', 'email')"
                     class="mb-0 pb-0 mb-0"
                     placeholder="Email"
                     outlined
@@ -36,14 +38,16 @@
                     v-model="email"
                     @keyup.enter="submit"
                   ></v-text-field>
-                  <p class="smallText mt-n3">We will never share your email with anyone</p>
+                  <p class="smallText mt-n3">
+                    We will never share your email with anyone
+                  </p>
                 </v-col>
 
                 <v-col cols="10" md="10" class="col-2 mb-0 pb-0">
                   <p class="mb-1">Password</p>
                   <v-text-field
                     class="mb-0"
-                    :rules="fieldRulesProp(true, 'password' , 'password')"
+                    :rules="fieldRulesProp(true, 'password', 'password')"
                     placeholder="Password"
                     outlined
                     v-model="password"
@@ -51,7 +55,36 @@
                     type="password"
                     @keyup.enter="submit"
                   ></v-text-field>
-                  <p class="text-right mt-n6">Forgot Password?</p>
+
+                  <div
+                    style="
+                      display: flex;
+                      justify-content: flex-end;
+                      margin-top: -15px;
+                    "
+                  >
+                    <router-link to="/forgot-password"
+                      >Forgot password?</router-link
+                    >
+                  </div>
+
+                  <div
+                    style="
+                      display: flex;
+                      justify-content: flex-start;
+                      margin-top: -15px;
+                      margin-bottom: -20px;
+                    "
+                  >
+                    <v-checkbox v-model="toc">
+                      <div slot="label">
+                        I agree with the
+                        <span style="color: #365899" @click.stop="tocView"
+                          >TOC</span
+                        >
+                      </div>
+                    </v-checkbox>
+                  </div>
                 </v-col>
 
                 <v-col cols="10" md="10" class="col-3">
@@ -60,8 +93,10 @@
                     block
                     color="primary"
                     class="white--text"
+                    :disabled="!toc"
                     @click.stop="submit"
-                  >Signup</v-btn>
+                    >Signup</v-btn
+                  >
                 </v-col>
 
                 <v-col cols="10" md="10">
@@ -70,20 +105,23 @@
 
                 <v-col cols="10" md="10" class="mb-n6 mt-0">
                   <div class="text-center">
-                    <router-link :to="{ name: 'Signin' }">Already have an account? Signin.</router-link>
+                    <router-link :to="{ name: 'Signin' }"
+                      >Already have an account? Signin.</router-link
+                    >
                   </div>
                 </v-col>
 
                 <v-col cols="10" md="10" class="mb-n4 mt-0">
-                  <p class="smallText mt-0 mb-0">All your activity will remain private</p>
+                  <p class="smallText mt-0 mb-0">
+                    All your activity will remain private
+                  </p>
                 </v-col>
 
                 <v-col cols="10" md="10" class="mb-0 mt-0">
                   <p class="tocText">
                     By signing in to your account, you agree to JOB ALERT’s
-                    <span
-                      class="tocInsideText"
-                    >Terms of Service</span> and consent to our
+                    <span class="tocInsideText">Terms of Service</span> and
+                    consent to our
                     <span class="tocInsideText">Cookie Policy</span> and
                     <span class="tocInsideText">Privacy Policy</span> .
                   </p>
@@ -94,6 +132,35 @@
         </v-row>
       </v-form>
     </v-container>
+
+    <div class="text-center">
+      <v-dialog v-model="dialog" width="500">
+        <v-card>
+          <v-card-title class="headline grey lighten-2">
+            Privacy Policy
+          </v-card-title>
+
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false">
+              I accept
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
   </div>
 </template>
 <script>
@@ -109,11 +176,16 @@ export default {
       email: "",
       password: "",
       username: "",
-
+      dialog: false,
       loading: false,
+      toc: false,
     };
   },
   methods: {
+    tocView() {
+      console.log("toc click");
+      this.dialog = true;
+    },
     submit() {
       if (!this.$refs.form.validate()) return;
       this.loading = true;
