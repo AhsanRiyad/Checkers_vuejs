e0456bc75b3545941e768a8d2329009192b57d9c
ahsanriyad <riyad298@gmail.com>
2020-10-11 19:05:05 +0600

    build added

--- dist/registerServiceWorker.js
+++ dist/registerServiceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.1c2cca4605067d8dfd3684ec95263956.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.99641c6736d16f8c2b560cfa5992070e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-console */
 
