78e4e7b582cb4a9afae2609f2c94aa67d9f18726
ahsanriyad <riyad298@gmail.com>
2020-10-13 16:03:08 +0600

    updated change password problem

--- src/views/JobAlart/Resume/WorkExperience.vue
+++ src/views/JobAlart/Resume/WorkExperience.vue
@@ -582,7 +582,7 @@ export default {
               })
               .catch((error) => {
                 this.$awn.alert("Failed!");
-                this.$route.history.push("/signin");
+                this.$router.history.push("/signin");
                 console.log(error);
               })
               .finally(() => {});
